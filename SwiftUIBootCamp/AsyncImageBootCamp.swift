3c0cd0ffb887a5a4f879f4754d231ae6abf63cdf
Uday Babariya <uday.babariya.77@gmail.com>
2023-12-28 08:12:16 +0530

    async image with phase

--- SwiftUIBootCamp/AsyncImageBootCamp.swift
+++ SwiftUIBootCamp/AsyncImageBootCamp.swift
@@ -12,14 +12,37 @@ struct AsyncImageBootCamp: View {
     let url = URL(string: "https://picsum.photos/350")
     
     var body: some View {
-//        AsyncImage(url: url)
-        AsyncImage(url: url) { downloadedImage in
-            downloadedImage
-        } placeholder: {
-            ProgressView()
+        //        AsyncImage(url: url)
+        VStack(spacing: 50) {
+            AsyncImage(url: url) { downloadedImage in
+                downloadedImage
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 100, height: 100)
+                    .cornerRadius(20)
+            } placeholder: {
+                ProgressView()
+            }
         }
-        .frame(width: 100, height: 100)
-
+        
+        AsyncImage(url: url) { phase in
+            switch phase {
+            case .empty :
+                ProgressView()
+            case .success(let downloadedImage):
+                downloadedImage
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 100, height: 100)
+                    .cornerRadius(20)
+            case .failure:
+                Image(systemName: "questionmark")
+                    .font(.headline)
+            default:
+                Image(systemName: "questionmark")
+                    .font(.headline)
+            }
+        }
     }
 }
 
