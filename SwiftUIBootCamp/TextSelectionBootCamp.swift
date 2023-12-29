36bf4add0fe83f69fc7a8ba287f9cc7161ae58ec
Uday Babariya <uday.babariya.77@gmail.com>
2023-12-29 08:15:58 +0530

    TextSelection BootCamp

---
+++ SwiftUIBootCamp/TextSelectionBootCamp.swift
@@ -0,0 +1,18 @@
+//
+//  TextSelectionBootCamp.swift
+//  SwiftUIBootCamp
+//
+//  Created by Uday on 29/12/23.
+//
+
+import SwiftUI
+
+struct TextSelectionBootCamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TextSelectionBootCamp()
+}
