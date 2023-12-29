4bc3ad86f3773fd0a6b8e38db9f876d88ce5ae6c
Uday Babariya <uday.babariya.77@gmail.com>
2023-12-29 08:38:04 +0530

    Button Styles Bootcamp

---
+++ SwiftUIBootCamp/ButtonStylesBootCamp.swift
@@ -0,0 +1,18 @@
+//
+//  ButtonStylesBootCamp.swift
+//  SwiftUIBootCamp
+//
+//  Created by Uday on 29/12/23.
+//
+
+import SwiftUI
+
+struct ButtonStylesBootCamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ButtonStylesBootCamp()
+}
