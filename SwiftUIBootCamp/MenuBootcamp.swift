40b0832b6c113e29a7cf979ab432aa4f03bfd89a
Uday Babariya <uday.babariya.77@gmail.com>
2024-01-08 08:51:38 +0530

    menu

---
+++ SwiftUIBootCamp/MenuBootcamp.swift
@@ -0,0 +1,32 @@
+//
+//  MenuBootcamp.swift
+//  SwiftUIBootCamp
+//
+//  Created by Uday on 08/01/24.
+//
+
+import SwiftUI
+
+struct MenuBootcamp: View {
+    var body: some View {
+        Menu("Click Me") {
+            Button("button 1") {
+                
+            }
+            
+            Button("button 2") {
+                
+            }
+            Button("button 3") {
+                
+            }
+            Button("button 4") {
+                
+            }
+        }
+    }
+}
+
+#Preview {
+    MenuBootcamp()
+}
